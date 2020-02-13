96695cf0d70282b557e5d2c48a918bcdfb7c3d60
Dauto Freitas <dauto_freitas@hotmail.com>
2020-02-12 22:52:32 -0300

    Melhorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './navegacao/menu/menu.component';
 import { HomeComponent } from './navegacao/home/home.component';
 import { FooterComponent } from './navegacao/footer/footer.component';
+import { ContatoComponent } from './institucional/contato/contato.component';
+import { SobreComponent } from './institucional/sobre/sobre.component';
+import { RouterModule } from '@angular/router';
+import { rootRouterConfig } from './app.routes';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    ContatoComponent,
+    SobreComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    [RouterModule.forRoot(rootRouterConfig, {useHash:false})]
   ],
   providers: [],
   bootstrap: [AppComponent]
